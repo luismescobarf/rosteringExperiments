9a8b2fa832b86b9a1d06dc61968f95bc12c772c6
luismescobarf <luismescobarf@gmail.com>
2022-02-03 16:22:33 -0500

    Ajuste de lector de instancias y generación de JSON correspondientes

--- cargaInstancias.py
+++ cargaInstancias.py
@@ -73,52 +73,51 @@ def cargarInstanciaMusliu(rutaArchivo):
                         'longMaxBloques' : arregloNumerico[3]
                     }
             
-            #Lectura de la duracion de los turnos
+            #Lectura de la longitud mínima y máxima de días libres
             f.readline() #Saltar línea vacía
             f.readline() #Saltar encabezado que hace referencia al valor (" Minimum and maximum length of days-off blocks  ")
-            casoEstudio['duracionTurnos'] = list()
             arregloLinea = f.readline().strip().split()
-            casoEstudio['duracionTurnos'].append({
-                        'DuracionMin'    : arregloLinea[0],
-                        'DuracionMax'    : arregloLinea[1],
-                                       
-                    }) 
-
+            casoEstudio['longitudDiasLibre'] = {
+                'longitudMinima' : int(arregloLinea[0]),
+                'longitudMaxima' : int(arregloLinea[1])
+            }           
+            
+            
             #Lectura de la longitud minima y maxima de los bloques de trabajo
             f.readline() #Saltar línea vacía
             f.readline() #Saltar encabezado que hace referencia al valor (" Minimum and maximum length of work blocks  ")
-            casoEstudio['longitudTurnos'] = list()
-            arregloLinea = f.readline().strip().split()
-            casoEstudio['longitudTurnos'].append({
-                        'LongitudMin'    : arregloLinea[0],
-                        'LongitudMax'    : arregloLinea[1],
-                                       
-                    }) 
-
+            arregloLinea = f.readline().strip().split()            
+            casoEstudio['longitudBloquesTrabajo'] = {
+                'longitudMinima' : int(arregloLinea[0]),
+                'longitudMaxima' : int(arregloLinea[1])
+            }            
         
-            #Lectura de la secuencia de turnos
+            #Lectura del número de secuencias no permitidas
             f.readline() #Saltar línea vacía
-            f.readline() #Saltar encabezado que hace referencia al valor ("Number of not allowed shift sequences: NrSequencesOfLength2, NrSequencesOfLength3:   ")
-            casoEstudio['secuenciaTurnos'] = list()
+            f.readline() #Saltar encabezado que hace referencia al valor ("Number of not allowed shift sequences: NrSequencesOfLength2, NrSequencesOfLength3:   ")            
             arregloLinea = f.readline().strip().split()
-            casoEstudio['secuenciaTurnos'].append({
-                        'SecuenciaDeLongitud1'    : arregloLinea[0],
-                        'SecuenciaDeLongitud2'    : arregloLinea[1],
-                                       
-                    }) 
-           
+            casoEstudio['numeroSecuenciasNoPermitidas'] = {
+                        'SecuenciaDeLongitud2'    : int(arregloLinea[0]),
+                        'SecuenciaDeLongitud3'    : int(arregloLinea[1]),                                       
+                    }           
 
             #Lectura de la secuencia de turnos no permitidos
             f.readline() #Saltar línea vacía
-            f.readline() #Saltar encabezado que hace referencia al valor ("Not allowed shift sequences ")
-            casoEstudio['SecuenciaTurnosNP'] = list()
-            for _ in range(casoEstudio['numeroTurnos']):
+            f.readline() #Saltar encabezado que hace referencia al valor ("Not allowed shift sequences ")          
+            casoEstudio['SecuenciaTurnosNP'] = list()            
+            for i in range(casoEstudio['numeroSecuenciasNoPermitidas']['SecuenciaDeLongitud2']):
                 arregloLinea = f.readline().strip().split()
                 casoEstudio['SecuenciaTurnosNP'].append({
                     'Turno 1'         : arregloLinea[0],
                     'Turno 2'         : arregloLinea[1],               
-                }) 
-            
+                })                
+            for i in range(casoEstudio['numeroSecuenciasNoPermitidas']['SecuenciaDeLongitud3']):
+                arregloLinea = f.readline().strip().split()
+                casoEstudio['SecuenciaTurnosNP'].append({
+                    'Turno 1'         : arregloLinea[0],
+                    'Turno 2'         : arregloLinea[1],               
+                    'Turno 3'         : arregloLinea[2],               
+                })         
 
     #Mensaje por si sucede algun error              
     except:
@@ -128,17 +127,46 @@ def cargarInstanciaMusliu(rutaArchivo):
     return casoEstudio
 
 
-#Sección Principal del codigo 
+#Sección Principal del codigo (uso de la librería)
 #--------------------------------------------------
 
-#Aca cargamos el archivo que vamos a utlizar 
-casoEstudio = cargarInstanciaMusliu("instancias/Example20.txt")
-#Aca imprimimos los datos 
-pp.pprint(casoEstudio)
+#Listado de instancias
+nombresInstancias = ["instancias/Example1.txt",
+                     "instancias/Example2.txt",
+                     "instancias/Example3.txt",
+                     "instancias/Example4.txt",
+                     "instancias/Example5.txt",
+                     "instancias/Example6.txt",
+                     "instancias/Example7.txt",
+                     "instancias/Example8.txt",
+                     "instancias/Example9.txt",
+                     "instancias/Example10.txt",
+                     "instancias/Example11.txt",
+                     "instancias/Example12.txt",
+                     "instancias/Example13.txt",
+                     "instancias/Example14.txt",
+                     "instancias/Example15.txt",
+                     "instancias/Example16.txt",
+                     "instancias/Example17.txt",
+                     "instancias/Example18.txt",
+                     "instancias/Example19.txt",
+                     "instancias/Example20.txt"]
 
+#nombresInstancias = ["instancias/Example12.txt"]
 
-#Convertir el diccionario a archivo JSON para facilitar la carga en corridas posteriores
-crearSobreescribir_JSON_Instancia(casoEstudio)
-
-
-
+#Carga de cada uno de los archivos del listado previo
+for rutaArchivo in nombresInstancias:   
+     
+    #Salida de diagnóstico
+    print("Caso que se está cargando: ",rutaArchivo)
+    
+    casoEstudio = cargarInstanciaMusliu(rutaArchivo)
+    
+    # # #Salida ordenada de diagnóstico
+    # pp.pprint(casoEstudio)
+    
+    #Salida de diagnóstico
+    print("Caso que se está escribiendo: ",rutaArchivo)
+    
+    #Convertir el diccionario a archivo JSON para facilitar la carga en corridas posteriores
+    crearSobreescribir_JSON_Instancia(casoEstudio)
---
+++ constructivoPark.py
@@ -0,0 +1,8 @@
+#Constructivo (Park, 2001) adaptado a Rostering -> Rotación de personal
+
+#Librerías
+import cargaInstancias
+import json
+import pprint as pp 
+
+
